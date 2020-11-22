653d4807d43623647ed9b93512f5902476e0cee2
Antonio <antonioareas@me.com>
2020-11-21 16:22:32 -0800

    Add project files.

---
+++ Lab2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lab2 {
+    class Program {
+        static void Main(string[] args) {
+
+            RollDice();
+            PrintNumbers(num1: 1, num2: 2, num3: 3);
+            Average(1, 2, 3, 4, 5, 6);
+
+        }
+
+        //optional parameter example
+        public static void RollDice(int sides = 6) {
+            Random random = new Random();
+            Console.WriteLine(random.Next(1, sides + 1));
+        }
+
+        //named parameter example
+        public static void PrintNumbers(int num2, int num3, int num1) {
+            Console.WriteLine("{0} {1} {2}", num1, num2, num3);
+        }
+
+        //variable number of parameters example
+        public static void Average(params int[] numbers) {
+            double total = 0;
+            foreach (int number in numbers) {
+                total += number;
+            }
+            Console.WriteLine(total / (double)numbers.Length);
+        }
+
+    }
+}
---
+++ Lecture 12 Labs/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Lecture_12_Labs {
+    class Program {
+        static void Main(string[] args) {
+
+            //create boynames and girlnames dictionary
+            Dictionary<string, int> boyNames = new Dictionary<string, int>();
+            Dictionary<string, int> girlNames = new Dictionary<string, int>();
+
+            //using space as delimiter character
+            char[] delimiterChars = { ' ' };
+
+            //create the boyNames dictionary based on text file
+            using (StreamReader reader = new StreamReader("boynames-1.txt")) {
+                string line = null;
+                while ((line = reader.ReadLine()) != null) {
+                    string[] lineData = line.Split(delimiterChars);
+                    boyNames.Add(lineData[0], Int32.Parse(lineData[1]));
+                } 
+            }
+
+            //create the girlNames dictionary based on textFile
+            using (StreamReader reader = new StreamReader("girlnames-1.txt")) {
+                string line = null;
+                while ((line = reader.ReadLine()) != null) {
+                    string[] lineData = line.Split(delimiterChars);
+                    girlNames.Add(lineData[0], Int32.Parse(lineData[1]));
+                }
+            }
+
+            //get name user wants and set to lowercase
+            Console.WriteLine("Please input a name to search for.");
+            string name = Console.ReadLine();
+            name = name.ToLower();
+
+            //variables for search
+            int rankBoys = 1;
+            int rankGirls = 1;
+            bool nameFoundBoys = false;
+            bool nameFoundGirls = false;
+
+            foreach (KeyValuePair<string, int> val in boyNames) {
+                if (val.Key.ToLower() == name) {
+                    Console.WriteLine("{0} is ranked {1} in popularity among boys with {2} namings", val.Key, rankBoys, val.Value);
+                    nameFoundBoys = true;
+                    break;
+                }
+                else {
+                    rankBoys++;
+                }
+            }
+
+            foreach (KeyValuePair<string, int> val in girlNames) {
+                if (val.Key.ToLower() == name) {
+                    Console.WriteLine("{0} is ranked {1} in popularity among girls with {2} namings", val.Key, rankGirls, val.Value);
+                    nameFoundGirls = true;
+                    break;
+                }
+                else {
+                    rankGirls++;
+                }
+            }
+
+            if (!nameFoundBoys) {
+                Console.WriteLine("{0} is not ranked among the top 1000 boy names", name);
+            }
+
+            if (!nameFoundGirls) {
+                Console.WriteLine("{0} is not ranked among the top 1000 girl names", name);
+            }
+
+        }
+    }
+}
